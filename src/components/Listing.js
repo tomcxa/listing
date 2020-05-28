8533f20d8802b399c7a3cb8334c1e9a4524e61c7
tomcxa <tomcxa@yandex.ru>
2020-05-28 12:57:03 +0300

    update Listing

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import ListingItem from './ListingItem'
 
 const Listing = ({ items }) => {
-    // const {
-    //     url,
-    //     listing_id,
-    //     MainImage,
-    //     title,
-    //     currency_code,
-    //     price,
-    //     quantity
-    // } = items
 
     return (
         <div className='item-list'>
-            {items.map((item) => {
-                console.log(item.title);
-                return (
+            {items.map((item) => (
                 <ListingItem
                     key={item.listing_id}
                     url={item.url}
@@ -28,19 +16,9 @@ const Listing = ({ items }) => {
                     price={item.price}
                     quantity={item.quantity}
                 />)
-            })}
+            )}
         </div>
     )
-}
-
-Listing.propTypes = {
-    url: PropTypes.string,
-    listingId: PropTypes.number,
-    img: PropTypes.string,
-    title: PropTypes.string,
-    currencyCode: PropTypes.string,
-    price: PropTypes.string,
-    quantity: PropTypes.number
 }
 
 export default Listing
